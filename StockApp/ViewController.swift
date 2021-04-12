478346fd68493a56da99b888fdbc8217720099e2
Kumari Ritu Pal <kumpal@publicisgroupe.net>
2021-04-12 22:34:12 +0530

    Initial Commit

---
+++ StockApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StockApp
+//
+//  Created by Kumari Ritu Pal on 12/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
